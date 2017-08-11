4483c92fd7416cd2badf8b01491263eeb731d3a8
Joona <joonatoona@digitalfishfun.com>
2017-08-10 20:58:46 -0700

    start CLI

---
+++ CLI.py
@@ -0,0 +1,16 @@
+from CurseAPI import CurseAPI
+
+motd = """
+  _|_|    _|      _|  _|      _|  
+_|    _|  _|_|  _|_|  _|_|  _|_|  
+_|    _|  _|  _|  _|  _|  _|  _|  
+_|    _|  _|      _|  _|      _|  
+  _|_|    _|      _|  _|      _|  
+
+        OpenMineMods V1.0
+"""
+
+print(motd)
+
+print("Initializing Curse API...")
+curse = CurseAPI()
