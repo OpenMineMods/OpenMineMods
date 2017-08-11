7d719449bed23c4329a82bb73de8615897f29a2c
Joona <joonatoona@digitalfishfun.com>
2017-08-10 20:51:46 -0700

    add docstrings

--- CurseAPI.py
+++ CurseAPI.py
@@ -6,13 +6,16 @@ from bs4 import BeautifulSoup
 useUserAgent = "Mozilla/5.0 (Windows NT 10.0; rv:50.0) Gecko/20100101 Firefox/50.0"
 
 class CurseAPI:
+    """Curse API"""
     def __init__(self):
         self.baseUrl = "https://mods.curse.com/mc-mods/minecraft"
         self.forgeUrl = "https://minecraft.curseforge.com"
+        # Set User Agent header for extra sneakyness
         self.session = requests.Session()
         self.session.headers.update({"User-Agent": useUserAgent})
 
     def getModList(self, version="", page=0):
+        """Get an array of `CurseProject`s"""
         parsed = self.get(params = {
             "filter-project-game-version": version,
             "page": page
@@ -22,28 +25,33 @@ class CurseAPI:
         return projects
 
     def getFiles(self, pid):
+        """Get an array of `CurseFile`s from a project ID"""
         parsed = self.get(path="/projects/{}/files".format(pid), host=self.forgeUrl)
         return [CurseFile(i) for i in parsed.select(".project-file-list-item")]
 
     def getVersionList(self):
+        """Get all versions availible on Curse"""
         parsed = self.get()
         options = parsed.select("#filter-project-game-version > option")
         return [i["value"] for i in options if i["value"] != ""]
 
     def get(self, params={}, path="", host=False):
+        """HTTP GET with HTML parsing"""
         if not host:
             host = self.baseUrl
         html = self.session.get(host+path, params=params).text
         return BeautifulSoup(html, "html.parser")
 
     def downloadFile(self, url, filepath):
+        """Download a file from `url` to `filepath`"""
         r = self.session.get(url, stream=True)
         with open(filepath, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024): 
+            for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
                     f.write(chunk)
 
 class CurseProject:
+    """Class for getting project information"""
     def __init__(self, element):
         self.el = element
 
@@ -64,6 +72,7 @@ class CurseProject:
         return self.el.select(selector)[index].contents[0]
 
 class CurseFile:
+    """Class for getting information from a file element"""
     def __init__(self, element):
         self.el = element
 
@@ -84,5 +93,3 @@ class CurseFile:
 
     def getContent(self, selector, index=0):
         return self.el.select(selector)[index].contents[0]
-
-curse = CurseAPI()
