59849822c45a0d53fd1e8c200a3e7de66eb34fc4
Joona <joonatoona@digitalfishfun.com>
2017-10-01 10:21:03 -0700

    2.0

--- API/CurseAPI.py
+++ API/CurseAPI.py
@@ -30,7 +30,7 @@ class CurseAPI:
           _|_|    _|      _|  _|      _|
     """
 
-    version = "1.1.0"
+    version = "2.0.0"
     baseUrl = "https://mods.curse.com"
     forgeUrl = "https://minecraft.curseforge.com"
 
