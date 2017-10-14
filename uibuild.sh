d8089fab4908448c55add61dad72f978e5804b12
Joona <joonatoona@digitalfishfun.com>
2017-10-13 21:06:19 -0700

    explicitly specify python3 (should fix #40)

--- uibuild.sh
+++ uibuild.sh
@@ -3,7 +3,7 @@
 for f in UI/*.ui; do
 	pyn=$(echo $f | sed 's/.ui/.py/' | sed 's/UI/GUI/')
 	echo "Building $f to $pyn"
-	python -m PyQt5.uic.pyuic $f -o $pyn
+	python3 -m PyQt5.uic.pyuic $f -o $pyn
 done
 
 pyrcc5 Assets/icons.qrc -o icons_rc.py
